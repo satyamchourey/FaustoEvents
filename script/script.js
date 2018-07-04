02e7e6877743123827dbc560f0dcca20922571e9
satyamchourey <satyamchourey26@github.com>
2018-07-04 19:59:00 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+// const figCaption = ["Online Events" , "Offline Events"]
+
+// const onlineEvents = document.getElementById("onlineEvents");
+// console.log(onlineEvents);
+// function onHover (){
+//     onlineEvents.style.opacity= "0.5";
+//     onlineEvents.
+// }
+
+// onlineEvents.addEventListener("mouseover" , onHover , false);
